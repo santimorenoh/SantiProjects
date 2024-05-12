cb979ab612890c163d776c41da9b447efac4b5bb
Santiago Moreno <santiagomoreno@Santiagos-MacBook-Air.local>
2024-05-12 16:56:40 -0500

    Course challenge done by Santi

--- 02 Curso Python Comprenhensions, Funciones y Manejo de Errores/app/challenge.py
+++ 02 Curso Python Comprenhensions, Funciones y Manejo de Errores/app/challenge.py
@@ -1,4 +1,5 @@
 import read_csv
+import charts
 
 
 data_path = './app/data.csv'
@@ -8,7 +9,16 @@ country = 'Argentina'
 
 for country_data in raw_data:
     if country in country_data['Country/Territory']:
-        filtered_data = list(filter(lambda item: item[0].isdigit(), list(country_data.keys())))
+        # filtered_data = list(filter(lambda item: item[0].isdigit(), list(country_data.keys())))
+        filtered_data = dict(filter(lambda item: item[0][0].isdigit(), country_data.items()))
+        years_list = list(map(lambda x: x.replace(' Population', ''),list(filtered_data.keys())))
+        population_list = list(map(lambda x: int(x), list(filtered_data.values())))
 
 print(filtered_data)
+print('')
+print(years_list)
+print('')
+print(population_list)
+
+charts.generate_bar_chart(years_list, population_list)
     
