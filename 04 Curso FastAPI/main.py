d1f4c445d43ecad5023a55348b07d1a54b20d366
santimorenoh <santiago_hoyos27@hotmail.com>
2024-06-17 21:02:04 -0500

    Swagger first steps

--- 04 Curso FastAPI/main.py
+++ 04 Curso FastAPI/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 
 app = FastAPI()
+app.title = 'My first FastAPI app'
+app.version = '0.0.1'
 
-@app.get('/')
+@app.get('/', tags = ['home'])
 def message():
-    return 'Hello World Two'
+    return 'Hello World Two - Probando 1 2 3 probando'
