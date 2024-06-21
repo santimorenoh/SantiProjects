a043d496b4a03f79fb3e03f39ff354beb5a8abc1
Santiago Moreno <Santiago.Moreno@corecard.com>
2024-06-21 11:08:29 -0700

    Adding GET, POST, PUT and DELETE methods

--- 04 Curso FastAPI/main.py
+++ 04 Curso FastAPI/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
 from fastapi.responses import HTMLResponse
 
 app = FastAPI()
@@ -34,9 +34,61 @@ def message():
 def get_movies():
     return movies
 
-@app.get('/movie/{id}', tags = ['movie'])
+@app.get('/movies/{id}', tags = ['movies'])
 def get_movie(id: int):
     for movie in movies:
         if movie["id"] == id:
             return movie
     return {}
+
+@app.get('/movies/', tags = ['movies'])
+def get_movies_by_category(category: str, year: int):
+    return [movie for movie in movies if movie["category"] == category]
+
+@app.post('/movies', tags = ['movies'])
+def create_movie(title: str = Body(), overview: str = Body(), year: str = Body(), rating: float = Body(), category: str = Body()):
+
+    max_id = 0
+    for movie in movies:
+        if movie["id"] > max_id:
+            max_id = movie["id"]
+    
+    id = max_id + 1
+
+    movies.append({
+
+        "id": id,
+        "title": title,
+        "overview": overview,
+        "year": year,
+        "rating": rating,
+        "category": category
+
+    })
+
+    return movies
+
+@app.put('/movies/{id}', tags = ['movies'])
+def update_movie(id: int, title: str = Body(), overview: str = Body(), year: str = Body(), rating: float = Body(), category: str = Body()):
+
+    for movie in movies:
+        if movie['id'] == id:
+            movie['title'] = title
+            movie['overview'] = overview
+            movie['year'] = year
+            movie['rating'] = rating
+            movie['category'] = category
+
+    return  movies
+
+
+@app.delete('/movies/{id}', tags = ['movies'])
+def delete_movie(id: int):
+
+    for movie in movies:
+        if movie['id'] == id:
+            movies.remove(movie)
+
+    return movies
+
+
