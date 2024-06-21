e2c60661ae8ef11784cfb1c1f72497877c5852ed
santimorenoh <santiago_hoyos27@hotmail.com>
2024-06-20 19:03:18 -0500

    Creating GET methods to obtain movies or a filtered movie

--- 04 Curso FastAPI/main.py
+++ 04 Curso FastAPI/main.py
@@ -1,9 +1,42 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 app.title = 'My first FastAPI app'
 app.version = '0.0.1'
 
+movies = [
+    {
+        "id": 1,
+        "title": "Avatar",
+        "overview": "Here the sinopsis",
+        "year": "2009",
+        "rating": 7.8,
+        "category": "Action"
+    },
+
+    {
+        "id": 2,
+        "title": "Other Avatar",
+        "overview": "Here the other sinopsis",
+        "year": "other 2009",
+        "rating": 7.8,
+        "category": "Action"
+    }
+]
+
+
 @app.get('/', tags = ['home'])
 def message():
-    return 'Hello World Two - Probando 1 2 3 probando'
+    return HTMLResponse('<h1>Hello World in HTML</h1>')
+
+@app.get('/movies', tags = ['movies'])
+def get_movies():
+    return movies
+
+@app.get('/movie/{id}', tags = ['movie'])
+def get_movie(id: int):
+    for movie in movies:
+        if movie["id"] == id:
+            return movie
+    return {}
